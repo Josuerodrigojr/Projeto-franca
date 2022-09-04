9ae4ef2f8605f6c7f3225f04b4d1811f73642b4b
Josuerodrigojr <98060884+Josuerodrigojr@users.noreply.github.com>
2022-09-04 11:13:25 -0300

    Atualizações

---
+++ projeto-franca/src/pages/js/Topo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/Topo.css'
+
+const Topo = () =>{
+    return (
+        <>
+        <p>hdjshdfjshdjksahdjsahdjhsajdhsajkd</p>
+        
+        </>
+    )
+}
+
+export default Topo;
